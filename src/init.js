88f057356b5428490d7925b0b84c5662108a1168
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-05-17 18:24:04 +0300

    update

--- src/init.js
+++ src/init.js
@@ -65,7 +65,7 @@ export default () => {
         watchedState.rssForm.valid = err.name !== 'ValidationError';
         watchedState.rssForm.error = err.isAxiosError
           ? 'form.errors.networkProblems'
-          : 'error';
+          : err.message;
         watchedState.rssForm.state = 'filling';
       });
   });
