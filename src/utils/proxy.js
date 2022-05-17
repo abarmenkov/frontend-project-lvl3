767aec6e23d858b9f96ba622a1a953a903bed97f
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-05-17 21:22:12 +0300

    update

--- src/utils/proxy.js
+++ src/utils/proxy.js
@@ -1,1 +1,1 @@
-export default (url) => new URL(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`).toString();
+export default (url) => new URL(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
